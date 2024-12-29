2943d20c78214f0f6e132b373c15e86bd964e04c
BhavyaG-1288 <gbhavya2017@gmail.com>
2024-12-30 00:32:46 +0530

    Half navbar created

---
+++ scripts/navbar.js
@@ -0,0 +1,13 @@
+
+    
+
+    let navbar =`  <div id ="navbar">
+            <a href ="Login.html">Login</a>
+            <a href ="Signup.html">Signup</a>
+        </div>`
+
+
+        //calling function by ID
+        document.getElemenrById("nav").innerHTML="navbar"
+
+
